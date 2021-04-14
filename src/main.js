440d61dc053cf59f2a4154ecdd1c0c8e0ef5dcb7
一个傻瓜 <1025474058@qq.com>
2021-04-14 10:56:23 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 import router from './router'
-import store from './store'
-
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import store from './store/index.js'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
+import '../node_modules/swiper/dist/css/swiper.css'
 Vue.config.productionTip = false
+Vue.use(VueAwesomeSwiper)
 
+// 本段更新
 new Vue({
   router,
   store,
